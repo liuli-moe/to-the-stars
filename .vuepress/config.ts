1b7e60277e0451cd2ab146ca7963dddabb64da6c
rxliuli <rxliuli@gmail.com>
2022-09-21 23:53:12 +0800

    chore: 添加 logo 和 favicon

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -12,6 +12,7 @@ export default defineUserConfig({
   title: '魔法少女小圆-飞向星空',
   description:
     '在经历了几个世纪的动荡之后，一个乌托邦式的 AI— 人类政府治理着地球，预示着后稀缺社会的来临和太空殖民的新时代。一次意外的接触却让科技更先进的敌对外星种族打破了和平，这迫使魔法少女们走出幕后，拯救人类文明。在这一切之中，志筑良子，一个普通的女孩，仰望着星空，好奇着她在宇宙中的归所。',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
 
   // 主题和它的配置
   theme: defaultTheme({
@@ -21,6 +22,8 @@ export default defineUserConfig({
     editLinkText: '在 GitHub 上编辑此页',
     lastUpdatedText: '上次更新',
     contributorsText: '贡献者',
+    logo: '/logo.png',
+    logoDark: '/logoDark.png',
   }),
   plugins: [
     googleAnalyticsPlugin({ id: 'G-F20H7RT1RM' }),

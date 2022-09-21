d0dfb03008f73f8a2b97fa4b2a60765d324eeb17
rxliuli <rxliuli@gmail.com>
2022-09-21 21:55:45 +0800

    refactor: 将 clearStrongAfterSpace 抽离为单独的插件

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,45 +1,10 @@
 import { defineUserConfig, defaultTheme, Plugin } from 'vuepress-vite'
-import MarkdownIt from 'markdown-it'
-import Token from 'markdown-it/lib/token'
 import { Hooks } from 'vuepress-vite'
 import data from './.temp/data.json'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { sitemapPlugin } from 'vuepress-plugin-sitemap2'
 import { giscusPlugin } from './plugins/vuepress-plugin-giscus'
-
-/**
- * 清理粗体之后的空格
- */
-function clearStrongAfterSpace(ends: string[]): MarkdownIt.PluginSimple {
-  return (md) => {
-    const renderInline = md.renderer.renderInline.bind(md.renderer)
-    md.renderer.renderInline = (tokens, options, env) => {
-      const checkTokenIndexs = tokens
-        .map((token, i) => {
-          return {
-            p:
-              token.type === 'strong_close' &&
-              ends.some((end) => tokens[i - 1].content.endsWith(end)) &&
-              i + 1 < tokens.length &&
-              tokens[i + 1].type === 'text',
-            i: i + 1,
-          }
-        })
-        .filter((item) => item.p)
-        .map((item) => item.i)
-      const newTokens: Token[] = tokens.map((token, i) => {
-        if (!checkTokenIndexs.includes(i)) {
-          return token
-        }
-        return {
-          ...token,
-          content: token.content.trimStart(),
-        } as Token
-      })
-      return renderInline(newTokens, options, env)
-    }
-  }
-}
+import { clearStrongAfterSpace } from './plugins/vuepress-plugin-clear-strong-after-space'
 
 export default defineUserConfig({
   // 站点配置
@@ -60,12 +25,7 @@ export default defineUserConfig({
   plugins: [
     googleAnalyticsPlugin({ id: 'G-F20H7RT1RM' }),
     sitemapPlugin({ hostname: 'https://tts.liuli.moe' }),
-    {
-      name: 'clearStrongAfterSpace',
-      extendsMarkdown(md, _app) {
-        md.use(clearStrongAfterSpace(['，', '。', '？', '！']))
-      },
-    } as Plugin & Hooks,
+    clearStrongAfterSpace(),
     giscusPlugin({
       repo: 'liuli-moe/to-the-stars', // required, string, format: user_name/repo_name
       repoId: 'R_kgDOG4H10w', // required, string, generate it on Giscus's website

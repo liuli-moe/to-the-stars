98acfd9b3c511ee89b8d286b34adf36ce7fe6eb7
rxliuli <rxliuli@gmail.com>
2023-03-18 15:41:27 +0800

    fix: update content extract

--- src/rss.ts
+++ src/rss.ts
@@ -53,21 +53,21 @@ import { readFile, writeFile } from 'fs/promises'
 function parseFile(file: string): Omit<Item, 'link' | 'date'> {
   const html = parse(file)
   console.log(html.querySelector('title').textContent)
+  const content = html.querySelector('.markdown')
+  content.removeChild(content.querySelector('h1'))
   return {
     title: html.querySelector('title').textContent,
     description: html.querySelector('meta[name="description"]').getAttribute('content'),
-    content: html.querySelector('.markdown').innerHTML,
+    content: content.innerHTML,
   }
 }
 
 async function main() {
   const distPath = path.resolve(__dirname, '../build')
 
-  const list = (
-    await FastGlob('books/**/*.html', {
-      cwd: distPath,
-    })
-  )
+  const list = await FastGlob('books/**/*.html', {
+    cwd: distPath,
+  })
   const posts: Item[] = await Promise.all(
     list.map(async (it) => {
       const file = await readFile(path.resolve(distPath, it), 'utf-8')
@@ -86,10 +86,10 @@ async function main() {
       '在经历了几个世纪的动荡之后，一个乌托邦式的 AI— 人类政府治理着地球，预示着后稀缺社会的来临和太空殖民的新时代。一次意外的接触却让科技更先进的敌对外星种族打破了和平，这迫使魔法少女们走出幕后，拯救人类文明。在这一切之中，志筑良子，一个普通的女孩，仰望着星空，好奇着她在宇宙中的归所。',
     id: 'https://tts.liuli.moe/',
     link: 'https://tts.liuli.moe/',
-    language: 'en', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    language: 'zh-CN', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
     image: 'https://tts.liuli.moe/cover.png',
     favicon: 'https://tts.liuli.moe/logo.png',
-    copyright: 'All rights reserved 2023, John Doe',
+    copyright: 'Copyright © 2023 rxliuli, Inc. Built with feed.',
     feedLinks: {
       json: 'https://tts.liuli.moe/json',
       atom: 'https://tts.liuli.moe/atom',
